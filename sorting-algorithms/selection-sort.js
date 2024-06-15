6764ce29e8040acb11be7189ebfb60795356a859
HasanOmar1 <r3domar1@gmail.com>
2024-06-15 11:26:01 +0300

    added selectionsort

---
+++ sorting-algorithms/selection-sort.js
@@ -0,0 +1,17 @@
+// O(n^2)
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      [arr[i], arr[min]] = [arr[min], arr[i]];
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort([5, 3, 4, 1, 2])); // [1,2,3,4,5]
