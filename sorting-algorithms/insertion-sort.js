16df93290a32678610ecef94db52a151dd9c0bbe
HasanOmar1 <r3domar1@gmail.com>
2024-06-18 19:22:53 +0300

    updated

--- sorting-algorithms/insertion-sort.js
+++ sorting-algorithms/insertion-sort.js
@@ -1,9 +1,9 @@
-function insertionSort(arr, compare = (a, b) => a - b) {
+function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let currentVal = arr[i];
     let j;
 
-    for (j = i - 1; j >= 0 && compare(currentVal, arr[j]) < 0; j--) {
+    for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j];
     }
 
@@ -11,3 +11,5 @@ function insertionSort(arr, compare = (a, b) => a - b) {
   }
   return arr;
 }
+
+// console.log(insertionSort([5, 3, 1, 9, 2]));
